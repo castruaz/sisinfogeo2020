25c7fb11d250e76df6b950d2d000c844f6d4df54
castruaz <castr@uaz.edu.mx>
2020-01-30 11:19:54 -0600

    va todo otra vez

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Bienvenido al lenguaje C#");
+
+        }
+    }
+}
